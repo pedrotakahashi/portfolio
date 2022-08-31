bbd4a29c7374aaf8073bc2eba45e18eb52dcc118
pedrotakahashi <pedroh.takahashi@gmail.com>
2022-08-31 12:01:18 -0300

    Atualização das paginas

--- src/Components/Content/ContentAbout.tsx
+++ src/Components/Content/ContentAbout.tsx
@@ -1,18 +1,14 @@
 import React from "react"
 import { Container, Row, Col, Card } from "react-bootstrap"
 import profilePicture from '../../Assets/images/mypic.jpeg'
-import maverick from '../../Assets/images/maverick.gif'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 
 const Content = () => {
-  const character = 'https://www.tibia.com/community/?name=Maverick+Destroyer';
-
   return (
     <Container className="content-contact">
-      <Row className="mx-auto">
-        <Col></Col>
+      <Row align="center">
         <Col>
           <Card className="card-about-me">
             <Card.Img variant="top" src={profilePicture} />
@@ -20,26 +16,11 @@ const Content = () => {
               <Card.Title>It's Me</Card.Title>
               <Card.Text>
                 Meu nome é Pedro Takahashi <br />
-                Sou desenvolvedor front end, com foco no uso em React. <br />
-                Fora daqui sou um aspirante a Bárbaro e Bardo <br />
-                Jogador assíduo de tibia, com um Elite Knight Lv 240 em Luminera
+                Sou desenvolvedor front end, com foco em React. <br />
+                Também, tenho noções de python, node.js e testes unitários com Jest
               </Card.Text>
             </Card.Body>
           </Card>
-        </Col>
-        <Col>
-          <Card className="card-tibia">
-            <Card.Img variant="top" src={maverick} />
-            <Card.Body>
-              <Card.Title>Maverick Destroyer</Card.Title>
-              <Card.Text>
-                Elite Knight - 242<br />
-                Luminera <br />
-
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
         </Col>
       </Row>
     </Container>
